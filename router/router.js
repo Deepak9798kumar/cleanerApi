77c645d81d6d7c974756909190c01c06ab6342c8
Deepak9798kumar <111009426+Deepak9798kumar@users.noreply.github.com>
2023-12-15 15:55:38 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,37 +1,34 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const tempDirectory = 'C:\\Windows\\Temp';
 
 // Define a route that triggers the file deletion
-router.get('/delete-files', (req, res) => {
-    // Read the contents of the temp directory
-    fs.readdir(tempDirectory, (err, files) => {
-        if (err) {
-            console.error('Error reading directory:', err);
-            res.status(500).send('Internal Server Error: Unable to read directory');
-            return;
-        }
+router.get('/delete-files', async (req, res) => {
+    try {
+        // Read the contents of the temp directory
+        const files = await fs.readdir(tempDirectory);
 
         // Delete each file
-        files.forEach(file => {
+        for (const file of files) {
             const filePath = path.join(tempDirectory, file);
+            try {
+                await fs.unlink(filePath);
+                console.log(`File ${filePath} deleted successfully`);
+            } catch (unlinkError) {
+                console.error(`Error deleting file ${filePath}:`, unlinkError.message);
+            }
+        }
 
-            fs.unlink(filePath, (unlinkErr) => {
-                if (unlinkErr) {
-                    console.error(`Error deleting file ${filePath}:`, unlinkErr);
-                } else {
-                    console.log(`File ${filePath} deleted successfully`);
-                }
-            });
-        });
-
-        // Send a response once all files are deleted
-        res.send('Files deleted successfully');
-    });
+        // Send a response once all deletions are attempted
+        res.send('Deletion attempted for all files');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send(`Internal Server Error: ${error.message}`);
+    }
 });
 
 module.exports = router;
