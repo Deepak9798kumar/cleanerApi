50e691ccb0b72643a4537b0c42a58a59e03d7fa9
Deepak9798kumar <111009426+Deepak9798kumar@users.noreply.github.com>
2023-12-15 15:23:54 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -6,24 +6,31 @@ const path = require('path');
 
 const tempDirectory = 'C:\\Windows\\Temp';
 
-// Read the contents of the temp directory
-fs.readdir(tempDirectory, (err, files) => {
-    if (err) {
-        console.error('Error reading directory:', err);
-        return;
-    }
+// Define a route that triggers the file deletion
+router.get('/delete-files', (req, res) => {
+    // Read the contents of the temp directory
+    fs.readdir(tempDirectory, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
 
-    // Delete each file
-    files.forEach(file => {
-        const filePath = path.join(tempDirectory, file);
+        // Delete each file
+        files.forEach(file => {
+            const filePath = path.join(tempDirectory, file);
 
-        fs.unlink(filePath, (unlinkErr) => {
-            if (unlinkErr) {
-                console.error(`Error deleting file ${filePath}:`, unlinkErr);
-            } else {
-                console.log(`File ${filePath} deleted successfully`);
-            }
+            fs.unlink(filePath, (unlinkErr) => {
+                if (unlinkErr) {
+                    console.error(`Error deleting file ${filePath}:`, unlinkErr);
+                } else {
+                    console.log(`File ${filePath} deleted successfully`);
+                }
+            });
         });
+
+        // Send a response once all files are deleted
+        res.send('Files deleted successfully');
     });
 });
 
